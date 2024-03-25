e5b532f33a8c030e26c068059f59789811bd5968
Bruno Staine <brunostaine@hotmail.com>
2024-03-25 10:52:39 -0300

    Desafio finalizado

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) {
-        System.out.printf("Hello World!");
+    public static void main(String[] args) throws Exception {
+
+        int numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "MARIO ANDRADE";
+        double saldo = 237.48;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+
+        String inputAgencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua conta !");
+
+        Integer inputNumero = Integer.parseInt(scanner.nextLine());
+
+        if (inputAgencia.equals(agencia) && inputNumero.equals(numero)) {
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        } else {
+            System.out.println("Não encontramos sua agência no sistema, tente novamente mais tarde!");
+        }
     }
 }
